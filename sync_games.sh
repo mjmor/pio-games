09e5fc557cb1ad77d02e8a23cef1411651d2abcd
Maxwell Morgan <mjmor@umich.edu>
2018-02-23 01:14:45 -0500

    update sync script

--- sync_games.sh
+++ sync_games.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-games=( "hellophaser" "tutorial" "adventure-math" )
+games=( "adventure-math" )
 
 sudo rsync -vrz index.html /var/www/html/index.html
-sudo rsync -vrz games-site.css /var/www/html/games-site.css
+sudo rsync -vrz css/games-site.css /var/www/html/css/games-site.css
 
 for game in "${games[@]}"; do
     echo "Syncing game $game..."
     sudo rsync -vrz ${game}/ /var/www/html/${game}/
-    sudo chmod -R a+rx /var/www/html/*
 done
+sudo chmod -R a+rx /var/www/html/*
